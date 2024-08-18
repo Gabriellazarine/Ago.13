5e6c99ca742c5bfb0c91fcd4f1aad990f63442ae
Gabriel Lazarine <gabriellazarinesilva@gmail.com>
2024-08-18 16:01:30 -0300

    Delete Flashcard/const findFlashcardIndexById = require('. directory

--- Flashcard/const findFlashcardIndexById = require('./findFlashcardIndexById.js
+++
@@ -1,8 +0,0 @@
-const data = require('./data');
-
-function findFlashcardIndexById(id) {
-  const { flashcards } = data;
-  return flashcards.findIndex(flashcard => flashcard.id === id);
-}
-
-module.exports = findFlashcardIndexById;
