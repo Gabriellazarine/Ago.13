7b3eaf3db77d3c7dcbfd850fb5268b6674bbc0d7
Gabriel Lazarine <gabriellazarinesilva@gmail.com>
2024-08-18 15:25:55 -0300

    Create atualizarFlashcard.js

---
+++ Flashcard/atualizarFlashcard.js
@@ -0,0 +1,21 @@
+const data = require('./data');
+
+function atualizarFlashcard(id, novaPergunta, novaResposta, novoBaralho) {
+  const { flashcards } = data;
+  const index = flashcards.findIndex(flashcard => flashcard.id === id);
+  if (index !== -1) {
+    flashcards[index] = {
+      id,
+      pergunta: novaPergunta,
+      resposta: novaResposta,
+      baralho: novoBaralho
+    };
+    console.log('Flashcard atualizado com sucesso!');
+    return flashcards[index];
+  } else {
+    console.log('Flashcard não encontrado.');
+    return null;
+  }
+}
+
+module.exports = atualizarFlashcard;
